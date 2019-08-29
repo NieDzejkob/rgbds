28912c59c29fb8635d50d8202e361abf642f443f
Jakub Kądziołka <kuba@kadziolka.net>
2019-08-29 19:16:32 +0200

    Make update-refs.sh also update the .out.pipe file

--- test/asm/update-refs.sh
+++ test/asm/update-refs.sh
@@ -4,6 +4,9 @@ fname=$(mktemp)
 for i in *.asm; do
 	../../rgbasm $i >$fname 2>&1
 	mv -f $fname ${i%.asm}.out
+
+	cat $i | ../../rgbasm - >$fname 2>&1
+	mv -f $fname ${i%.asm}.out.pipe
 done
 
 rm -f $fname
